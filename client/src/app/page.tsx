a41fb2cdb592db55c101bc9f689278a90523a176
karanvirsb <karanvirboparai123@gmail.com>
2023-07-25 11:43:27 -0400

    added text area

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { useEffect, useReducer, useRef, useState } from "react";
 import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
 
 function Chat() {
   const [messageHistory, setMessageHistory] = useState<string[]>([]);
+  const message = useRef<string>("");
   const { lastMessage, sendMessage, readyState } = useWebSocket(
     "ws://localhost:8000/socket"
   );
@@ -16,14 +18,16 @@ function Chat() {
     if (readyState !== 1) return;
     if (lastMessage === null) return;
 
-    if (typeof lastMessage !== "string") return;
-    setMessageHistory((prev) => prev.concat(lastMessage));
+    setMessageHistory((prev) => prev.concat(lastMessage.data));
   }, [lastMessage, readyState]);
   return (
     <div>
       <div>
         <h1>Messages</h1>
-        <Button>Click me to send hello</Button>
+        <Textarea
+          onChange={(e) => (message.current = e.target.value)}
+        ></Textarea>
+        <Button>Send Message</Button>
       </div>
       {messageHistory}
     </div>
