04dd2db07bae2e0f210dcca08214728f7ba0c65f
karanvirsb <karanvirboparai123@gmail.com>
2023-07-25 11:05:46 -0400

    added web socket

--- server/main.go
+++ server/main.go
@@ -3,9 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 func main() {
+	http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			return
+		}
+
+		for {
+			msgType, msg, err := conn.ReadMessage()
+
+			if err != nil {
+				return
+			}
+
+			fmt.Printf("%v -- sent message: %v", conn.RemoteAddr(), string(msg))
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Server request")
 	})
