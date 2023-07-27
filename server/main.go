d81ef69d87c860056dc5ff838a9f15ad13a7e8e6
karanvirsb <karanvirboparai123@gmail.com>
2023-07-27 09:33:16 -0400

    moving things back

--- server/main.go
+++ server/main.go
@@ -4,11 +4,70 @@ import (
 	"chat_app_server/socketEvents"
 	"fmt"
 	"net/http"
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
+
+type Message[T any] struct {
+	Data      T        `json:"data,omitempty"`
+	EventName string   `json:"eventName,omitempty"`
+	Room      []string `json:"room,omitempty"`
+}
+
+type Socket struct {
+	id    string
+	rooms []string
+	conn  *websocket.Conn
+}
+
+type Connections struct {
+	mu    sync.Mutex
+	conns []Socket
+}
+
+var allowedOrigins []string = []string{"http://localhost:3000"}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		found := false
+		origin := r.Header.Get("Origin")
+
+		for _, o := range allowedOrigins {
+			if o == origin {
+				found = true
+				break
+			}
+		}
+
+		return found
+	},
+}
+
+func (c *Connections) addConnection(conn Socket) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.conns = append(c.conns, conn)
+}
+
+func (socket *Socket) addRooms(rooms []string) {
+	socket.rooms = append(socket.rooms, rooms...)
+}
+
+func generateId() string {
+	return uuid.NewString()
+}
+
+var connections = Connections{
+	conns: []Socket{},
+}
 
 func main() {
 
-	http.HandleFunc("/socket", socketEvents.CaptureSocketEvents)
+	http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
+		socketEvents.CaptureSocketEvents(w, r)
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Server request")
 	})
