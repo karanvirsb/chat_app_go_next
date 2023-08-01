949368b483daf9962be2e0f9abd8bb35ab1712d7
karanvirsb <karanvirboparai123@gmail.com>
2023-08-01 16:38:33 -0400

    added interface and method to read

--- server/websockets/socket.go
+++ server/websockets/socket.go
@@ -2,7 +2,17 @@ package websockets
 
 import "github.com/gorilla/websocket"
 
+type ISocket interface {
+	read(*interface{}) (Message, error)
+}
+
 type Socket struct {
 	Id   string
 	Conn *websocket.Conn
+}
+
+func (s *Socket) read() (Message, error) {
+	jsonMessage := Message{}
+	err := s.Conn.ReadJSON(&jsonMessage)
+	return jsonMessage, err
 }
