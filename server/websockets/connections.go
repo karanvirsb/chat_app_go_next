d1c16cca8cfb2baf0048e44feee04cfb8ac9395b
karanvirsb <karanvirboparai123@gmail.com>
2023-08-01 16:27:06 -0400

    added connection struct and method

--- server/websockets/connections.go
+++ server/websockets/connections.go
@@ -1,1 +1,14 @@
 package websockets
+
+import "sync"
+
+type Connections struct {
+	Mu    sync.Mutex
+	Conns []Socket
+}
+
+func (c *Connections) AddConnection(conn Socket) {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	c.Conns = append(c.Conns, conn)
+}
