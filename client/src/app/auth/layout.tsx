7e95b8767a4d16677b3c12045911bff42ba8453e
karanvirsb <karanvirboparai123@gmail.com>
2023-08-02 12:17:19 -0400

    added styling

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="grid place-content-center min-h-screen bg-gray-200">
+      {children}
+    </div>
+  );
 }
