422305210ead35051d3688e5a292ee2cdbfe41a9
karanvirsb <karanvirboparai123@gmail.com>
2023-08-04 15:23:31 -0400

    created sidebar

--- client/src/components/home/sidebar.tsx
+++ client/src/components/home/sidebar.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { rooms } from "@/app/page";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
 
 export function Sidebar() {
-  return <div>Sidebar</div>;
+  const [activeRoom, setActiveRoom] = useState("1");
+  return (
+    <div>
+      <h1>Go Chat</h1>
+      {rooms.map((room) => {
+        return (
+          <Button
+            key={room}
+            variant="link"
+            className={activeRoom === room ? "bg-blue-400" : ""}
+            onClick={() => {
+              setActiveRoom(room);
+            }}
+          >
+            {room}
+          </Button>
+        );
+      })}
+    </div>
+  );
 }
