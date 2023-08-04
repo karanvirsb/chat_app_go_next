00ec8ff0cc87874efe9115a365cac0d768fbfafe
karanvirsb <karanvirboparai123@gmail.com>
2023-08-04 15:46:34 -0400

    added dummy user and styling

--- client/src/components/home/members.tsx
+++ client/src/components/home/members.tsx
@@ -3,12 +3,22 @@ import React from "react";
 export function Members() {
   // fetch members from server
   // listen to events on updating the members
-  const users: string[] = [];
+  const users: string[] = ["username"];
   return (
-    <div>
-      <h1>Online Users</h1>
+    <div className="px-2">
+      <h1 className="text-[26px] font-semibold text-center">Online Users</h1>
       {users.map((user) => {
-        return <div key={user}>{user}</div>;
+        return (
+          <>
+            <div
+              className="rounded-md px-2 py-1 flex justify-center items-center gap-4"
+              key={user}
+            >
+              {user}
+              <span className="w-4 h-4 bg-green-400 rounded-full"></span>
+            </div>
+          </>
+        );
       })}
     </div>
   );
