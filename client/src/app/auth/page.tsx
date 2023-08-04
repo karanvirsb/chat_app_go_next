53a2aff61bfb5fb84d754741b5b0d6d48681bdc9
karanvirsb <karanvirboparai123@gmail.com>
2023-08-04 08:52:00 -0400

    created form

--- client/src/app/auth/page.tsx
+++ client/src/app/auth/page.tsx
@@ -1,8 +1,15 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useAuthContext } from "@/context/AuthContext";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface Inputs {
@@ -10,22 +17,46 @@ interface Inputs {
 }
 
 export default function Auth() {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const form = useForm<Inputs>();
+
   const { updateUsername } = useAuthContext();
+  const [usernameErr, setUsernameErr] = useState("");
+
   return (
     <div className="min-w-sm max-w-md w-full shadow-md px-4 py-2 rounded-md bg-white">
       <h1 className="text-center font-semibold text-3xl mb-4">Login</h1>
-      <form
-        onSubmit={handleSubmit(handleOnSubmit)}
-        className="flex flex-col gap-4"
-      >
-        <Input type="text" {...register("username")} placeholder="Username" />
-        <Button type="submit">Login</Button>
-      </form>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleOnSubmit)}
+          className="flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input type="text" placeholder="example" {...field} />
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          ></FormField>
+          <p>{usernameErr}</p>
+          <Button type="submit">Login</Button>
+        </form>
+      </Form>
     </div>
   );
 
   function handleOnSubmit(data: Inputs) {
+    console.log(data);
+    if (data.username.length < 3) {
+      setUsernameErr("Username must be 3 characters long.");
+      return;
+    }
     updateUsername(data.username);
   }
 }
