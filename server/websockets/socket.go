d0e7a4cd8eeda7ba97fc25426675501525efedf9
karanvirsb <karanvirboparai123@gmail.com>
2023-08-08 15:13:30 -0400

    added mutex for syncing writes

--- server/websockets/socket.go
+++ server/websockets/socket.go
@@ -1,6 +1,10 @@
 package websockets
 
-import "github.com/gorilla/websocket"
+import (
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
 
 type ISocket interface {
 	read(*interface{}) (Message[any], error)
@@ -10,10 +14,17 @@ type Socket struct {
 	Id       string
 	Username string
 	Conn     *websocket.Conn
+	mu       sync.Mutex
 }
 
 func (s *Socket) read() (Message[any], error) {
 	jsonMessage := Message[any]{}
 	err := s.Conn.ReadJSON(&jsonMessage)
 	return jsonMessage, err
+}
+
+func (s *Socket) writeJSON(v interface{}) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.Conn.WriteJSON(v)
 }
