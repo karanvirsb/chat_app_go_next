8c4b4cc30b07d313f78e235531ce2e9b9e03be21
karanvirsb <karanvirboparai123@gmail.com>
2023-08-08 11:21:21 -0400

    added join room case

--- server/websockets/captureEvents.go
+++ server/websockets/captureEvents.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 )
 
-type Message struct {
-	Data      any    `json:"data,omitempty"`
+type Message[T any] struct {
+	Data      T      `json:"data,omitempty"`
 	EventName string `json:"eventName,omitempty"`
 	Room      string `json:"room,omitempty"`
 }
@@ -36,28 +36,38 @@ func CaptureSocketEvents(socket *Socket, Connections *Connections, rooms *map[st
 
 		switch jsonMessage.EventName {
 		case "join_room":
-			var usernameCheck interface{} = jsonMessage.Data
-			s, ok := usernameCheck.(MessageJoinRoom)
-			if !ok {
-				fmt.Println("Error does not contain username - line 42 - captureEvents.go")
-			}
-			socket.Username = s.Username
-			// add room to socket and add socket to rooms map
-			if foundRoom := DoesRoomExist(*rooms, jsonMessage.Room); foundRoom != nil {
-				fmt.Println("Found room")
+			fmt.Printf("Join room case: %v\n", jsonMessage.Room)
+			msg := Message[MessageJoinRoom]{}
+			err := socket.Conn.ReadJSON(&msg)
 
+			if err != nil {
+				fmt.Printf("Error not a json - \n%v\n - \n%v\n", msg, err)
+				return
+			}
+
+			socket.Username = msg.Data.Username
+			// add room to socket and add socket to rooms map
+			fmt.Printf("Does Room %v Exist: %v\n", msg.Room, DoesRoomExist(rooms, msg.Room))
+			if foundRoom := DoesRoomExist(rooms, msg.Room); foundRoom != nil {
+				fmt.Printf("Found room: %v\n", msg.Room)
+				// wg.Add(1)
 				go foundRoom.RunRoom()
 				foundRoom.Register <- socket
 				defer func() { foundRoom.Unregister <- socket }()
 			} else {
-				fmt.Println("New room")
-				newRoom := NewRoom(jsonMessage.Room)
-
+				fmt.Printf("New room: %v\n", msg.Room)
+				newRoom := NewRoom(msg.Room)
+				// wg.Add(1)
 				go newRoom.RunRoom()
 				defer func() { newRoom.Unregister <- socket }()
 				newRoom.Register <- socket
-				(*rooms)[jsonMessage.Room] = *newRoom
+				(*rooms)[msg.Room] = *newRoom
 			}
+			//joinRoomEvent(socket, rooms)
+			// wg.Wait()
+
+			// wg.Done()
+			fmt.Printf("Ended join room: %v", msg.Room)
 
 		case "send_message_to_room":
 			if room, ok := (*rooms)[jsonMessage.Room]; ok {
@@ -78,10 +88,14 @@ func CaptureSocketEvents(socket *Socket, Connections *Connections, rooms *map[st
 
 	}
 }
-func DoesRoomExist(rooms map[string]Room, roomName string) *Room {
-	r, ok := rooms[roomName]
+func DoesRoomExist(rooms *map[string]Room, roomName string) *Room {
+	r, ok := (*rooms)[roomName]
 	if !ok {
 		return nil
 	}
 	return &r
 }
+
+// func joinRoomEvent(socket *Socket, rooms *map[string]Room) {
+// 	// wg := sync.WaitGroup{}
+// }
