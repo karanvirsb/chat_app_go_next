d713aca7c8a56a94fbcb93111170f39b16eadcf4
karanvirsb <karanvirboparai123@gmail.com>
2023-08-08 15:15:30 -0400

    moved joined room and fixed reading message

--- server/websockets/captureEvents.go
+++ server/websockets/captureEvents.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"sync"
 
 	"log"
 	"strings"
@@ -19,66 +20,40 @@ type MessageJoinRoom struct {
 	Username string `json:"username,omitempty"`
 }
 
+var (
+	buf    bytes.Buffer
+	logger = log.New(&buf, "capture socket events logger: ", log.Default().Flags())
+)
+var count int = 0
+
 func CaptureSocketEvents(socket *Socket, Connections *Connections, rooms *map[string]Room) {
-	var (
-		buf    bytes.Buffer
-		logger = log.New(&buf, "capture socket events logger: ", log.Default().Flags())
-	)
+	buf.Grow(30000)
+	// wg := sync.WaitGroup{}
 	for {
 
 		jsonMessage, err := socket.read()
 
 		if err != nil {
-
 			logger.Printf("\nJson Message Error: %v\n", err)
-			if strings.Contains(err.Error(), "close") {
+			go printBuffer()
+			if strings.Contains(err.Error(), "close") || strings.Contains(err.Error(), "RSV") {
+				socket.Conn.WriteJSON("")
 				break
 			}
 			continue
 		}
 
 		msg, _ := json.Marshal(jsonMessage)
-
 		logger.Printf("\nJSON message: %v\n", string(msg))
-
+		go printBuffer()
 		switch jsonMessage.EventName {
 		case "join_room":
-			logger.Printf("Join room case: %v\n", jsonMessage.Room)
-			msg := Message[MessageJoinRoom]{}
-			err := socket.Conn.ReadJSON(&msg)
-
-			if err != nil {
-				logger.Printf("Error not a json - \n%v\n - \n%v\n", msg, err)
-				return
-			}
-
-			socket.Username = msg.Data.Username
-			// add room to socket and add socket to rooms map
-			logger.Printf("Does Room %v Exist: %v\n", msg.Room, DoesRoomExist(rooms, msg.Room))
-			if foundRoom := DoesRoomExist(rooms, msg.Room); foundRoom != nil {
-				logger.Printf("Found room: %v\n", msg.Room)
-				// wg.Add(1)
-				go foundRoom.RunRoom()
-				foundRoom.Register <- socket
-				defer func() { foundRoom.Unregister <- socket }()
-			} else {
-				logger.Printf("New room: %v\n", msg.Room)
-				newRoom := NewRoom(msg.Room)
-				// wg.Add(1)
-				go newRoom.RunRoom()
-				defer func() { newRoom.Unregister <- socket }()
-				newRoom.Register <- socket
-				(*rooms)[msg.Room] = *newRoom
-			}
-			//joinRoomEvent(socket, rooms)
-			// wg.Wait()
-
-			// wg.Done()
-			logger.Printf("Ended join room: %v", msg.Room)
-
+			count++
+			// wg.Add(count)
+			go joinRoomEvent(socket, rooms, &msg)
 		case "send_message_to_room":
 			if room, ok := (*rooms)[jsonMessage.Room]; ok {
-				room.Broadcast <- &jsonMessage
+				room.Broadcast <- msg
 			}
 
 		case "send_message_to_all":
@@ -86,16 +61,21 @@ func CaptureSocketEvents(socket *Socket, Connections *Connections, rooms *map[st
 				err := socket.Conn.WriteJSON(string(msg))
 				if err != nil {
 					logger.Printf("\nError while sending message: %v\n", err)
+					go printBuffer()
 					continue
 				}
 			}
 		default:
 			socket.Conn.WriteMessage(1, []byte("Error: That event does not exist"))
 		}
-
-		fmt.Println(&buf)
 	}
 }
+
+func printBuffer() {
+	fmt.Println(&buf)
+	buf.Reset()
+}
+
 func DoesRoomExist(rooms *map[string]Room, roomName string) *Room {
 	r, ok := (*rooms)[roomName]
 	if !ok {
@@ -104,6 +84,45 @@ func DoesRoomExist(rooms *map[string]Room, roomName string) *Room {
 	return &r
 }
 
-// func joinRoomEvent(socket *Socket, rooms *map[string]Room) {
-// 	// wg := sync.WaitGroup{}
-// }
+func joinRoomEvent(socket *Socket, rooms *map[string]Room, message *[]byte) {
+	wg := sync.WaitGroup{}
+	msg := Message[MessageJoinRoom]{}
+	err := json.Unmarshal(*message, &msg)
+
+	if err != nil {
+		logger.Printf("Error not a json - \n%v\n - \n%v\n", msg, err)
+		if strings.Contains(err.Error(), "close") {
+			wg.Done()
+			return
+		}
+
+	}
+	logger.Printf("Join room case: %v\n", msg.Room)
+
+	socket.Username = msg.Data.Username
+	// add room to socket and add socket to rooms map
+	logger.Printf("Does Room %v Exist: %v\n", msg.Room, DoesRoomExist(rooms, msg.Room))
+	if foundRoom := DoesRoomExist(rooms, msg.Room); foundRoom != nil {
+		logger.Printf("Found room: %v\n", msg.Room)
+		wg.Add(1)
+		go foundRoom.RunRoom()
+		foundRoom.Register <- socket
+		defer func() { foundRoom.Unregister <- socket }()
+	} else {
+		logger.Printf("New room: %v\n", msg.Room)
+		newRoom := NewRoom(msg.Room)
+		wg.Add(1)
+		(*rooms)[msg.Room] = *newRoom
+
+		go newRoom.RunRoom()
+		newRoom.Register <- socket
+		defer func() { newRoom.Unregister <- socket }()
+	}
+	wg.Wait()
+
+	defer func() {
+		wg.Done()
+		logger.Printf("Ended join room: %v", msg.Room)
+	}()
+
+}
