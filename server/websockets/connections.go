d06bce3d0176a3bee6df60b37ec117f410aed0a1
karanvirsb <karanvirboparai123@gmail.com>
2023-08-09 10:48:01 -0400

    added remove connections

--- server/websockets/connections.go
+++ server/websockets/connections.go
@@ -1,6 +1,8 @@
 package websockets
 
-import "sync"
+import (
+	"sync"
+)
 
 type Connections struct {
 	Mu    sync.Mutex
@@ -11,4 +13,19 @@ func (c *Connections) AddConnection(conn *Socket) {
 	c.Mu.Lock()
 	defer c.Mu.Unlock()
 	c.Conns = append(c.Conns, conn)
+}
+
+func (c *Connections) RemoveConnection(conn *Socket) {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+
+	index := 0
+
+	for i, s := range c.Conns {
+		if s.Id == conn.Id {
+			index = i
+		}
+	}
+
+	c.Conns = append(c.Conns[:index], c.Conns[index+1:]...)
 }
