78fa471df88d82b09f26e4ca01c7dfa6df7832a6
karanvirsb <karanvirboparai123@gmail.com>
2023-08-09 10:28:04 -0400

    added WebSocketContext

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AuthContextProvider } from "@/context/AuthContext";
+import { WebSocketContextProvider } from "@/context/WebSocketContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <time dateTime="2023-08-02" suppressHydrationWarning />
-        <AuthContextProvider>{children}</AuthContextProvider>
+        <AuthContextProvider>
+          <WebSocketContextProvider>{children}</WebSocketContextProvider>
+        </AuthContextProvider>
         <Toaster />
       </body>
     </html>
