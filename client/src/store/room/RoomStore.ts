7d46fa9a6ba9515f6d55cf45bfab6140632a58f5
karanvirsb <karanvirboparai123@gmail.com>
2023-08-09 11:08:26 -0400

    updated to string

--- client/src/store/room/RoomStore.ts
+++ client/src/store/room/RoomStore.ts
@@ -1,13 +1,13 @@
 import { StateCreator } from "zustand";
 
 export interface IRoomSlice {
-  initialRoom: number;
-  setActiveRoom: (room: number) => void;
+  initialRoom: string;
+  setActiveRoom: (room: string) => void;
 }
 
 export const createRoomSlice: StateCreator<IRoomSlice, [], [], IRoomSlice> = (
   set
 ) => ({
-  initialRoom: 1,
+  initialRoom: "1",
   setActiveRoom: (room) => set((state) => ({ initialRoom: room })),
 });
