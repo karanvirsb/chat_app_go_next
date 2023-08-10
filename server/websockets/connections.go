b5ef89b6371493c7fd0390b4abc1a6ca39bdc75a
karanvirsb <karanvirboparai123@gmail.com>
2023-08-10 11:12:30 -0400

    added method to notify users of someone leaving

--- server/websockets/connections.go
+++ server/websockets/connections.go
@@ -1,6 +1,8 @@
 package websockets
 
 import (
+	"encoding/json"
+	"fmt"
 	"sync"
 )
 
@@ -28,4 +30,21 @@ func (c *Connections) RemoveConnection(conn *Socket) {
 	}
 
 	c.Conns = append(c.Conns[:index], c.Conns[index+1:]...)
+}
+
+type UserLeftMessage struct {
+	Id string `json:"id"`
+}
+
+func (c *Connections) NotifyUsersOfLeave(s *Socket) {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	leftMessage, err := json.Marshal(Message[UserLeftMessage]{Data: UserLeftMessage{Id: s.Id}, EventName: "user_disconnected"})
+	if err != nil {
+		fmt.Printf("Left Message Error: %v", err)
+		return
+	}
+	for _, s := range c.Conns {
+		s.writeJSON(string(leftMessage))
+	}
 }
