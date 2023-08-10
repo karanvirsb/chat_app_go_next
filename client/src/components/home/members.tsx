7f15c6f4a1f4f2b4113989e84a48595b3d0e4dbf
karanvirsb <karanvirboparai123@gmail.com>
2023-08-10 12:34:32 -0400

    added connected users event

--- client/src/components/home/members.tsx
+++ client/src/components/home/members.tsx
@@ -1,36 +1,59 @@
-import React from "react";
+"use client";
+import { useWebSocketContext } from "@/context/WebSocketContext";
+import React, { useEffect, useState } from "react";
 
 type Users = Array<{ username: string; id: string }>;
 
-type ReturnData<T> =
-  | {
-      success: true;
-      data: T;
-    }
-  | { success: false; error: unknown };
+// type ReturnData<T> =
+//   | {
+//       success: true;
+//       data: T;
+//     }
+//   | { success: false; error: unknown };
 
-async function getUsers(): Promise<ReturnData<Users>> {
-  const res = await fetch("http://localhost:8000/users");
+// async function getUsers(): Promise<ReturnData<Users>> {
+//   const res = await fetch("http://localhost:8000/users");
 
-  if (!res.ok) {
-    return { success: false, error: "Failed to fetch users" };
-  }
+//   if (!res.ok) {
+//     return { success: false, error: "Failed to fetch users" };
+//   }
 
-  return { success: true, data: (await res.json()) as Users };
-}
+//   return { success: true, data: (await res.json()) as Users };
+// }
 
-export async function Members() {
+export function Members() {
   // fetch members from server
-  const users = await getUsers();
+  // const users = getUsers();
   // listen to events on updating the members
+  const [users, setUsers] = useState<Users>([]);
+  const websocketHook = useWebSocketContext();
+
+  useEffect(() => {
+    if (websocketHook === null) return;
+
+    const websocket = websocketHook.websocketHook.getWebSocket();
+
+    if (websocket === null) return;
+
+    websocket.addEventListener("message", (e) => {
+      const jsonMessage = JSON.parse(JSON.parse((e as any).data));
+      console.log(jsonMessage);
+
+      if (jsonMessage.eventName === "connected_users") {
+        setUsers(jsonMessage.data.users);
+      }
+    });
+  }, [websocketHook?.websocketHook.getWebSocket()]);
+
+  if (websocketHook?.websocketHook === null) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="px-2 bg-brand">
       <h1 className="text-[26px] font-semibold text-center">Online Users</h1>
-      {!users.success ? (
-        <p>{typeof users.error === "string" && users.error}</p>
-      ) : (
-        users.data.map((user) => {
+      {users?.map((user) => {
+        if (user.id.length !== 0) {
           return (
             <div
               className="rounded-md px-2 py-1 flex justify-center items-center gap-4"
@@ -40,8 +63,8 @@ export async function Members() {
               <span className="w-4 h-4 bg-green-400 rounded-full"></span>
             </div>
           );
-        })
-      )}
+        }
+      })}
     </div>
   );
 }
