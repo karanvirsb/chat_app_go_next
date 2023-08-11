0dfc4e072e5dc7cb17108306563a8ef6f7eee4ec
karanvirsb <karanvirboparai123@gmail.com>
2023-08-11 16:56:58 -0400

    added message union

--- client/src/types/messages/messageTypes.ts
+++ client/src/types/messages/messageTypes.ts
@@ -1,18 +1,32 @@
-export const messageEvents = {
-  SEND_MESSAGE_TO_ROOM_EVENT: "send_message_to_room",
-  USER_ONLINE_EVENT: "user_online",
-  USER_DISCONNECTED_EVENT: "user_disconnected",
-  CONNECTED_USERS_EVENT: "connected_users",
+import { UserSentMessage } from "./isUserMessage";
+
+export type messageEvents = {
+  SEND_MESSAGE_TO_ROOM_EVENT: "send_message_to_room";
+  USER_ONLINE_EVENT: "user_online";
+  USER_DISCONNECTED_EVENT: "user_disconnected";
+  CONNECTED_USERS_EVENT: "connected_users";
 };
 
-export const messageActions = {
-  JOIN_ROOM: "join_room",
+export type messageActions = {
+  JOIN_ROOM: "join_room";
 };
 
 export type Users = Array<{ username: string; id: string }>;
 
-export interface Message<T> {
-  data?: T;
-  room?: string;
-  eventName?: string;
-}
+export type UserStatus = { username: string; id: string };
+
+export type JoinRoom = {
+  username: string;
+  rooms: string[];
+};
+
+export type Message =
+  | {
+      eventName: messageEvents["SEND_MESSAGE_TO_ROOM_EVENT"];
+      data: UserSentMessage;
+      room: string;
+    }
+  | { eventName: messageEvents["CONNECTED_USERS_EVENT"]; data: Users }
+  | { eventName: messageEvents["USER_DISCONNECTED_EVENT"]; data: UserStatus }
+  | { eventName: messageEvents["USER_ONLINE_EVENT"]; data: UserStatus }
+  | { eventName: messageActions["JOIN_ROOM"]; data: JoinRoom };
