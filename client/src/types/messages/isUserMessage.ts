1244070cc120e7fb2087414a9c2027d8a64d4bb6
karanvirsb <karanvirboparai123@gmail.com>
2023-08-11 14:43:57 -0400

    created isUserMessage validator

---
+++ client/src/types/messages/isUserMessage.ts
@@ -0,0 +1,15 @@
+export type UserSentMessage = {
+  text: string;
+  username: string;
+  time: number;
+};
+
+export function isUserMessage(data: unknown): data is Message<UserSentMessage> {
+  const typedData = data as Message<UserSentMessage>;
+  return (
+    typedData.data !== undefined &&
+    typedData.data.text !== undefined &&
+    typedData.data.username !== undefined &&
+    typedData.data.time !== undefined
+  );
+}
