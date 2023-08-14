945c731d6d3e49bba2e2538ea5f0695437fc0ea4
karanvirsb <karanvirboparai123@gmail.com>
2023-08-14 09:19:56 -0400

    added checks

--- client/src/types/messages/isMessage.ts
+++ client/src/types/messages/isMessage.ts
@@ -1,8 +1,15 @@
 import { Message } from "./messageTypes";
 
-export function isMessage<T>(data: unknown): data is Message<T> {
-  const d = data as Message<T>;
+export function isMessage(data: unknown): data is Message {
+  const d = data as Message;
   return (
-    d.data !== undefined && d.eventName !== undefined && d.room !== undefined
+    d.data !== undefined &&
+    d.eventName !== undefined &&
+    (d.eventName === "connected_users" ||
+      d.eventName === "join_room" ||
+      d.eventName === "send_message_to_room" ||
+      d.eventName === "user_disconnected" ||
+      d.eventName === "user_online" ||
+      d.eventName === "user_connected")
   );
 }
