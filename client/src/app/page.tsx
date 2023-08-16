322b4a3a313c6ec921156a572a3631932b5975c9
karanvirsb <karanvirboparai123@gmail.com>
2023-08-16 11:39:05 -0400

    removed rooms

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,8 +2,6 @@ import { Chat } from "@/components/home/chat";
 import { Members } from "@/components/home/members";
 import { Sidebar } from "@/components/home/sidebar";
 
-export const rooms = ["1", "2", "3"];
-
 export default function Home() {
   return (
     <main className="grid sm:grid-cols-1 md:grid-cols-[1fr_4fr_1fr] gap-4 min-h-screen">
