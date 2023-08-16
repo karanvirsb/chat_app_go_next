2f3397c27049e4707168447f6c396eed98dfa0e5
karanvirsb <karanvirboparai123@gmail.com>
2023-08-16 11:40:18 -0400

    added user slice

--- client/src/store/GoChatStore.ts
+++ client/src/store/GoChatStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { IRoomSlice, createRoomSlice } from "./room/RoomStore";
+import { IUserStore, createUsersSlice } from "./users/UsersStore";
 
-export const useChatStore = create<IRoomSlice>()(
+export const useChatStore = create<IRoomSlice & IUserStore>()(
   devtools((...a) => ({
     ...createRoomSlice(...a),
+    ...createUsersSlice(...a),
   }))
 );
