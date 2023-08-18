18de0a792d191875e298e60ab317841ecedde288
karanvirsb <karanvirboparai123@gmail.com>
2023-08-18 11:03:59 -0400

    added events

--- client/src/components/home/members.tsx
+++ client/src/components/home/members.tsx
@@ -8,54 +8,26 @@ export function Members() {
   const users = useChatStore((state) => state.users);
   const setUsers = useChatStore((state) => state.setUsers);
 
-  // useEffect(() => {
-  //   if (websocketHook === null) return;
-
-  //   const websocket = websocketHook.getWebSocket();
-
-  //   if (websocket === null) return;
-  //   websocket.addEventListener("message", listener);
-
-  //   function listener(e: any) {
-  //     const jsonMessage = JSON.parse(JSON.parse((e as any).data));
-  //     // console.log(jsonMessage.eventName);
-  //     if (!isMessage(jsonMessage)) return;
-
-  //     if (jsonMessage.eventName === "connected_users") {
-  //       // setUsers((prev) => [...prev, ...jsonMessage.data.users]);
-  //     } else if (jsonMessage.eventName === "user_disconnected") {
-  //       setUsers((prev) => {
-  //         const newUsers = prev.filter(
-  //           (user) => user.id !== jsonMessage.data.id
-  //         );
-  //         return newUsers;
-  //       });
-  //     } else if (jsonMessage.eventName === "user_connected") {
-  //       setUsers((prev) => {
-  //         return [
-  //           ...prev,
-  //           { id: jsonMessage.data.id, username: jsonMessage.data.username },
-  //         ];
-  //       });
-  //     }
-  //   }
-
-  //   return () => {
-  //     websocket.removeEventListener("message", listener);
-  //   };
-  // }, [websocketHook.getWebSocket()]);
   useEffect(() => {
-    console.log(users);
-  }, [users]);
+    eventEmitter.on("members", (jsonMessage: Message) => {
+      if (jsonMessage.eventName === "connected_users") {
+        setUsers(jsonMessage.data.users);
+      } else if (jsonMessage.eventName === "user_disconnected") {
+        const newUsers = users.filter(
+          (user) => user.id !== jsonMessage.data.id
+        );
 
-  useEffect(() => {
-    eventEmitter.on("members", (data: Message) => {
-      if (data.eventName === "connected_users") {
-        console.log("setting data", data.data.users);
-        setUsers(data.data.users);
+        setUsers(newUsers);
+      } else if (jsonMessage.eventName === "user_connected") {
+        const newUsers = [
+          ...users,
+          { id: jsonMessage.data.id, username: jsonMessage.data.username },
+        ];
+
+        setUsers(newUsers);
       }
     });
-  }, [setUsers]);
+  }, [setUsers, users]);
 
   return (
     <div className="px-2 bg-brand">
