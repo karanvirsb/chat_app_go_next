9c041fdcccd09ecdc7f002ffb52d23b16e49d810
karanvirsb <karanvirboparai123@gmail.com>
2023-08-18 09:12:51 -0400

    created a socket message handler

---
+++ client/src/handlers/socketMessageHandler.ts
@@ -0,0 +1,29 @@
+import { Message } from "@/types/messages/messageTypes";
+
+export function emitEvents(message: Message) {
+  switch (message.eventName) {
+    case "connected_users":
+      emitEventToMembers(message);
+      break;
+    case "user_disconnected":
+      emitEventToChat(message);
+      emitEventToMembers(message);
+      break;
+    case "user_connected":
+      emitEventToMembers(message);
+      break;
+    case "send_message_to_room":
+      emitEventToChat(message);
+      break;
+    case "user_online":
+      emitEventToChat(message);
+      break;
+    case "join_room":
+      break;
+    default:
+      break;
+  }
+}
+
+function emitEventToMembers(message: Message) {}
+function emitEventToChat(message: Message) {}
