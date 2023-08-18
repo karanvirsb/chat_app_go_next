6f0455a9969160f6e58eca0a283559e565f0c391
karanvirsb <karanvirboparai123@gmail.com>
2023-08-18 16:18:42 -0400

    added topbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,16 @@
 import { Chat } from "@/components/home/chat";
 import { Members } from "@/components/home/members";
 import { Sidebar } from "@/components/home/sidebar";
+import Topbar from "@/components/home/topbar";
 
 export default function Home() {
   return (
     <main className="grid sm:grid-cols-1 md:grid-cols-[1fr_4fr_1fr] gap-4 min-h-screen">
       <Sidebar></Sidebar>
-      <Chat></Chat>
+      <div className="flex flex-col">
+        <Topbar />
+        <Chat></Chat>
+      </div>
       <Members></Members>
     </main>
   );
