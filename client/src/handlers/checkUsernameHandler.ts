8031d5e860d77ec683c99f0d383ce1a71a5bc028
karanvirsb <karanvirboparai123@gmail.com>
2023-08-18 08:52:48 -0400

    created check username handler

---
+++ client/src/handlers/checkUsernameHandler.ts
@@ -0,0 +1,18 @@
+"use server";
+
+type ReturnData = { success: true } | { success: false; error: string };
+
+export async function handleCheckUsername(
+  username: string
+): Promise<ReturnData> {
+  const data = await fetch("http://localhost:8000/check/username", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username }),
+    method: "POST",
+  });
+
+  return await data.json();
+}
