de685c460238a0750fc0a8cc8cd4f5a8c527c8ad
karanvirsb <karanvirboparai123@gmail.com>
2023-08-18 16:49:17 -0400

    added toggle on smaller screen

--- client/src/components/home/sidebar.tsx
+++ client/src/components/home/sidebar.tsx
@@ -4,7 +4,12 @@ import { Button } from "../ui/button";
 import { useChatStore } from "@/store/GoChatStore";
 import { useAuthContext } from "@/context/AuthContext";
 
-export function Sidebar() {
+type Props = {
+  menuOpen: boolean;
+  toggleMenu: () => void;
+};
+
+export function Sidebar({ toggleMenu, menuOpen }: Props) {
   const rooms = useChatStore((state) => state.rooms);
   const setActiveRoom = useChatStore((state) => state.setActiveRoom);
   const setNotification = useChatStore((state) => state.setNotification);
@@ -12,7 +17,26 @@ export function Sidebar() {
 
   return (
     <div className="px-2 bg-brand flex flex-col lg:w-[300px] md:w-[250px] sm:fixed sm:inset-0 md:relative">
-      <h1 className="text-[32px] font-semibold text-center">Go Chat</h1>
+      <div className="flex justify-center items-center gap-4">
+        <h1 className="text-[32px] font-semibold text-center">Go Chat</h1>
+        {menuOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 hidden sm:block"
+            onClick={toggleMenu}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        ) : null}
+      </div>
       <div className="flex flex-col gap-4 mt-4">
         {rooms.map((room) => {
           return (
