45b9169681ac493b2fa0b7f2994d0c1b31f12d36
karanvirsb <karanvirboparai123@gmail.com>
2023-08-21 12:32:15 -0400

    moved check username handler

--- server/main.go
+++ server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"chat_app_server/handler"
 	"chat_app_server/websockets"
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -33,7 +32,9 @@ func main() {
 	router.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
 		handler.UsersRequestHandler(w, r, &connections)
 	})
-	router.HandleFunc("/check/username", checkUsernameRequestHandler).Methods("POST")
+	router.HandleFunc("/check/username", func(w http.ResponseWriter, r *http.Request) {
+		handler.CheckUsernameRequestHandler(w, r, &connections)
+	}).Methods("POST")
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Server request")
@@ -41,39 +42,4 @@ func main() {
 
 	handler := c.Handler(router)
 	http.ListenAndServe(":8000", handler)
-}
-
-func checkUsernameRequestHandler(w http.ResponseWriter, r *http.Request) {
-	type MessageResponse struct {
-		Success bool   `json:"success"`
-		Error   string `json:"error,omitempty"`
-	}
-	type RequestBody struct {
-		Username string `json:"username"`
-	}
-	var b RequestBody
-	err := json.NewDecoder(r.Body).Decode(&b)
-	if err != nil {
-		fmt.Printf("Error checking name %v\n", err)
-		json.NewEncoder(w).Encode(MessageResponse{Success: false, Error: "Server Error: Bad Request"})
-		return
-	}
-
-	foundUsername := false
-
-	for _, conn := range connections.Conns {
-		if conn.Username == b.Username {
-			foundUsername = true
-			break
-		}
-	}
-
-	if foundUsername {
-		w.WriteHeader(http.StatusAccepted)
-
-		json.NewEncoder(w).Encode(MessageResponse{Success: false, Error: "Username already exists"})
-		return
-	} else {
-		json.NewEncoder(w).Encode(MessageResponse{Success: true})
-	}
 }
